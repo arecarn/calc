3b67ded75544d02b8b305a612d427d653480781f
Ryan Carney <arecarn@gmail.com>
2016-08-23 18:39:45 -0700

    Add a tokenizer

---
+++ calc.py
@@ -0,0 +1,32 @@
+import re
+from collections import OrderedDict
+
+TOKEN_MAP = OrderedDict([
+    (r'\d+([.]\d+)?|.\d+', 'num'),
+    (r'[*]', 'mul'),
+    (r'/', 'mul'),
+    (r'\-', 'add'),
+    (r'\+', 'add'),
+    (r'.*', 'unknown'),
+])
+
+def tokenize(input_string, tokens):
+    output = []
+    string = input_string
+    while len(string) != 0:
+        for regex_str, token_id in tokens.items():
+            match = re.match(regex_str, string)
+            if match != None:
+                match_str = match.group()
+                match_str_len = len(match_str)
+                output.append((match.group(), token_id))
+                print("before="+string)
+                string = string[match_str_len:]
+                print("after="+string)
+    return output
+
+if __name__ == '__main__':
+    while True:
+        user_input = input('> ')
+        output = tokenize(user_input, TOKEN_MAP)
+        print(output)
